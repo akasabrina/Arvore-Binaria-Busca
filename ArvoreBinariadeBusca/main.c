52f666497b582f96a6f8816952a40ffed0197f5f
Sabrina Gaspar <75753093+akasabrina@users.noreply.github.com>
2022-09-26 10:55:41 -0300

    Update main.c

--- ArvoreBinariadeBusca/main.c
+++ ArvoreBinariadeBusca/main.c
@@ -18,27 +18,47 @@ tree *Tree(int cont, tree *esq, tree *dir){
 	return nova;
 }
 
-void printa(tree *arvore, int p){
-	if (arvore != NULL){
+void printa(tree *arv, int p){
+	if (arv != NULL){
 		int i;
 		for(i=0; i<p; i++){
 			printf("->");
 		}
-		printf("%d \n", arvore->cont);
-		printa(arvore->esq, p+1);
-		printa(arvore->dir, p+1);
+		printf("%d \n", arv->cont);
+		printa(arv->esq, p+1);
+		printa(arv->dir, p+1);
 	}
 }
 
-int main(int argc, char *argv[]) {
-	tree *arvore = Tree(1,
-					Tree(2, 
-						Tree(3, NULL, NULL), 
-						Tree(4, NULL, NULL)),
-					Tree(5, 
-						Tree(6, NULL, NULL), 
-						Tree(7, NULL, NULL)));
+tree *insere (tree *arv, int cont){
+	if (arv == NULL) return Tree(cont, NULL, NULL);
+	if (arv->cont > cont) arv->esq = insere(arv->esq, cont);
+	else arv->dir = insere(arv->dir, cont);
+	return arv;
+}
+
+tree *busca(tree* arv, int k){
+	if (arv == NULL || arv->cont == k) return arv;
+	if (arv->cont > k) return busca(arv->esq, k);
+	else return busca(arv->dir, k);
+}
+
+int main() {
+	tree *arvore = NULL;
+	arvore = insere(arvore,10);
+	arvore = insere(arvore,7);
+	arvore = insere(arvore,15);
+	arvore = insere(arvore,3);
+	arvore = insere(arvore,11);
 	
 	printa(arvore, 0);
+	printf("\n");
+	
+	int k = 11;
+	printf("Buscando o numero %d\n",k);
+	
+	if(busca(arvore, k) == NULL) printf("Nao achou!!!\n");
+	else printf("Achou!!!\n");
+	
 	return 0;
 }
